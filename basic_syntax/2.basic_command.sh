b409ca32ae15113f4f87fb3233b011714f9dcad0
JungJunHwan <qoqkd0221@naver.com>
2024-11-28 15:11:11 +0900

    저장

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -15,3 +15,34 @@ git commit
 
 # add와 commit을 동시에
 git commit -am "커밋메시지"
+
+# commit 이력 확인(local 레포의 이력)
+git log
+
+# 로그 한줄로 간결히 확인
+git log --oneline
+
+# 커밋 이력을 그래프 형태로 조회
+git log --graph
+
+# Head 하단의 로드만 보이는 것이 아닌, 모든 로그 조회
+# Head는 현재 checkcout 하고있는 커밋을 가리킴
+git log --all
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+
+# 충돌 발생시 무시하고 강제 push
+git push origin main --force
+
+# pull은 원격의 커밋 사항을 로컬로 다운로드(working directory까지 반영)
+git pull origin main
+
+# 특정 commit ID로
+git checkout 커밋ID
+
+# working directory작업 및 staging area에서의 취소는 UI로 대체
+
+# 가장 최신의 커밋 취소 : working directory changes까지로 reset
+git reset head~1
+git reset^
