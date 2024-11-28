26995dc9cdcb7484080f1ddae3ccb0077570dcf7
JungJunHwan <qoqkd0221@naver.com>
2024-11-28 14:09:36 +0900

    vi 모드 테스트 1)수정사항1 2)수정사항2 3)수정사항3

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -5,3 +5,10 @@ git status
 # add는 staging area로 변경사항을 이동함을 의미
 git add .
 git add 파일(경로포함)
+
+# commit을 통해 메세지 제목과 메시지 내용을 커밋 이력으로 작성
+git commit -m "메시지 제목" -m "메시지 내용"
+
+# git commit만 하고 엔터 실행시 vi모드
+# 첫줄 : 제목, 두번째줄부터 : 내용
+git commit
